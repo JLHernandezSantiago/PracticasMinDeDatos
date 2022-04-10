f3ec0285ab066306c47dbde8372b3150e7189221
JLHernandezSantiago <54382715+JLHernandezSantiago@users.noreply.github.com>
2022-04-10 17:39:17 -0500

    Practica 7 - Predicciones
    Predicciones de las Stats de las próximas Tdolls que sean agregadas al juego, de acuerdo al año de producción o diseño en la vida real, o como personajes propios del juego.

---
+++ practica 7/practica 7.py
@@ -0,0 +1,155 @@
+#PRACTICA 7 / PREDICCCIONES
+#FUE REALIZADO EN UN JUPYTER NOTEBOOK PORQUE TUVE PROBLEMAS CON LAS LIBRERIAS QUE NO ESTAN EN PATH
+#EN MI COMPUTADORA, ADEMAS DE QUE ME ANDABA FALLANDO MUCHO EL VISUAL STUDIO CODE
+#GRAFICOS GENERADOS: LOS GUARDE PARA QUE SE PUEDAN VER LA EVIDENCIA
+#Pd: La predicciones presentadas aplica en general, porque no siempre se sabe si las proximas armas
+#vayan a ser del mismo tipo (AR/SMG/MG/SG/HG/RF) a las mas recientes producidas en
+# la vida real/añadidas en el juego como originales
+
+#PRCATICA 6 - REGRESIÓN LINEAL
+# Tratamiento de datos
+# ==============================================================================
+import pandas as pd
+import numpy as np
+
+# Gráficos
+# ==============================================================================
+import matplotlib.pyplot as plt
+from matplotlib import style
+import seaborn as sns
+
+# Preprocesado y modelado
+# ==============================================================================
+from scipy.stats import pearsonr
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+# Configuración matplotlib
+# ==============================================================================
+plt.rcParams['image.cmap'] = "bwr"
+#plt.rcParams['figure.dpi'] = "100"
+plt.rcParams['savefig.bbox'] = "tight"
+style.use('ggplot') or plt.style.use('ggplot')
+
+# Configuración warnings
+# ==============================================================================
+import warnings
+warnings.filterwarnings('ignore')
+
+# Datos, de Dataset
+# ==============================================================================
+from google.colab import drive
+drive.mount("/content/gdrive")
+
+datos = pd.read_csv('/content/gdrive/My Drive/Colab Notebooks/Characters_GF_V1.3.csv')
+
+a = datos['Health_Points']
+b = datos['Damage']
+c = datos['ACC']
+d = datos['Evasion']
+e = datos['R_o_F']
+f = datos['Year_Prod']
+x = datos['Index_Tdoll']
+
+# Gráfico
+# ==============================================================================
+fig, ax = plt.subplots(figsize=(6, 3.84))
+
+datos.plot(
+    x    = 'Year_Prod', #variable tipo date
+    y    = 'R_o_F', #Intercambiando entre columnas a ~ e
+    c    = 'firebrick',
+    kind = "scatter",
+    ax   = ax
+)
+ax.set_title('Distribución de Stat en Año de produccion');
+
+# Correlación lineal entre las dos variables
+# ==============================================================================
+corr_test = pearsonr(x = datos['Year_Prod'], y =  datos['R_o_F'])
+print("Coeficiente de correlación de Pearson: ", corr_test[0])
+print("P-value: ", corr_test[1])
+
+# División de los datos en train y test
+# ==============================================================================
+X = datos[['Year_Prod']] #variable de tiempo 
+y = datos['R_o_F']
+
+X_train, X_test, y_train, y_test = train_test_split(
+                                        X.values.reshape(-1,1),
+                                        y.values.reshape(-1,1),
+                                        train_size   = 0.8,
+                                        random_state = 1234,
+                                        shuffle      = True
+                                    )
+
+# Creación del modelo
+# ==============================================================================
+modelo = LinearRegression()
+modelo.fit(X = X_train.reshape(-1, 1), y = y_train)
+
+# Información del modelo
+# ==============================================================================
+print("Intercept:", modelo.intercept_)
+print("Coeficiente:", list(zip(X.columns, modelo.coef_.flatten(), )))
+print("Coeficiente de determinación R^2:", modelo.score(X, y))
+
+# Error de test del modelo 
+# ==============================================================================
+predicciones = modelo.predict(X = X_test)
+print(predicciones[0:3,])
+
+rmse = mean_squared_error(
+        y_true  = y_test,
+        y_pred  = predicciones,
+        squared = False
+       )
+print("")
+print(f"El error (rmse) de test es: {rmse}")
+
+# División de los datos en train y test
+# ==============================================================================
+X = datos[['Year_Prod']] #Variable de año de produccion
+y = datos['R_o_F']
+
+X_train, X_test, y_train, y_test = train_test_split(
+                                        X.values.reshape(-1,1),
+                                        y.values.reshape(-1,1),
+                                        train_size   = 0.8,
+                                        random_state = 1234,
+                                        shuffle      = True
+                                    )
+
+# Creación del modelo utilizando matrices como en scikitlearn
+# ==============================================================================
+# A la matriz de predictores se le tiene que añadir una columna de 1s para el intercept del modelo
+X_train = sm.add_constant(X_train, prepend=True)
+modelo = sm.OLS(endog=y_train, exog=X_train,)
+modelo = modelo.fit()
+print(modelo.summary())
+
+# Intervalos de confianza para los coeficientes del modelo
+# ==============================================================================
+modelo.conf_int(alpha=0.05)
+
+# Predicciones con intervalo de confianza del 95%
+# ==============================================================================
+predicciones = modelo.get_prediction(exog = X_train).summary_frame(alpha=0.05)
+predicciones['x'] = X_train[:, 1]
+predicciones['y'] = y_train
+predicciones = predicciones.sort_values('x')
+
+# Gráfico del modelo
+# ==============================================================================
+fig, ax = plt.subplots(figsize=(6, 3.84))
+
+ax.scatter(predicciones['x'], predicciones['y'], marker='o', color = "gray")
+ax.plot(predicciones['x'], predicciones["mean"], linestyle='-', label="OLS")
+ax.plot(predicciones['x'], predicciones["mean_ci_lower"], linestyle='--', color='red', label="95% CI")
+ax.plot(predicciones['x'], predicciones["mean_ci_upper"], linestyle='--', color='red')
+ax.fill_between(predicciones['x'], predicciones["mean_ci_lower"], predicciones["mean_ci_upper"], alpha=0.1)
+ax.legend();
